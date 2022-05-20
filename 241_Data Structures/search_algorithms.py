610e64960f188ba09ed149454e148ec96009a679
jpandya <54954865+jatanjay@users.noreply.github.com>
2022-05-19 21:54:24 -0400

    Delete 241_Data Structures directory

--- 241_Data Structures/search_algorithms.py
+++
@@ -1,9 +0,0 @@
-# # selection sort
-# def selection_sort(array):
-# 	for i in range(len(array)):
-# 		minimum = i
-# 		for j in range(i + 1, len(array)):
-# 			if array[j] < array[minimum]:
-# 				minimum = j
-# 		array[i], array[minimum] = array[minimum], array[i]
-# 	print(array)
